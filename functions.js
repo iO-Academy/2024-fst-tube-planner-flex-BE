400abb82b150abf13295ab763ade18b64ccfbc58
Carl Ollington <Carl.Ollington@outlook.com>
2024-10-15 14:50:56 +0100

    working fetch and route

--- functions.js
+++ functions.js
@@ -1,12 +1,10 @@
 const {dbConnection} = require("./db");
-const mysql = require('promise-mysql')
-
 
 const getAllStations = async (req, res) => {
     const db = await dbConnection;
     try {
-        const stations = db.query(
-            'SELECT DISTINCT `code`, `name`, `ID` FROM stations Order By `name` ASC')
+        const stations = await db.query(
+            'SELECT DISTINCT `code`, `name` FROM stations Order By `name` ASC')
         res.status(200).json({message: "Successfully Retrieved Stations.", data: stations})
     } catch (e) {
         res.status(500).json({message: "Unexpected Error", data: []})
